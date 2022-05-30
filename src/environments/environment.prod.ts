489cf389bf2d4ba7f4799dd491a4aeb7cddcaa37
sawta71 <sawtaakhade@gmail.com>
2022-05-30 18:38:27 +0530

    api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl : "http://codetentacles-006-site36.htempurl.com/api/",
-  apiUrl : "http://209.126.10.42/moonsea/api/",
+  apiUrl : "http://codetentacles-006-site36.htempurl.com/api/",
+  // apiUrl : "http://209.126.10.42/moonsea/api/",
   defaultProfilePic : "img/profile.png",
   configFile : 'testnet',
   bscTestnetScan:"https://testnet.bscscan.com/tx/"
